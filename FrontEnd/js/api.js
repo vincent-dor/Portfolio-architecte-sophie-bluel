088a78ff06bd528e70bc48dfffbe1b9affe0b819
DOREE VINCENT <vincent.doree01@gmail.com>
2024-01-24 16:23:32 +0100

    refactor: code and description for api.js, auth.js

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -1,32 +1,21 @@
 // api.js - Gère les appels aux API pour récupérer les catégories et les œuvres
 
-// Fonction pour récupérer les œuvres depuis l'API
-export const fetchWorks = async () => {
-  // Effectue une requête pour récupérer les œuvres
-  const response = await fetch("http://localhost:5678/api/works");
-  
-  // Vérifie si la réponse de la requête est OK, sinon lance une erreur avec le statut HTTP
+// Fonction générique pour effectuer une requête API
+const fetchData = async (url, options = {}) => {
+  const response = await fetch(url, options);
+
   if (!response.ok) {
     throw new Error(`Erreur HTTP! Statut : ${response.status}`);
   }
-  
-  // Retourne les données des œuvres au format JSON
+
   return response.json();
 };
 
+// Fonction pour récupérer les œuvres depuis l'API
+export const fetchWorks = () => fetchData("http://localhost:5678/api/works");
+
 // Fonction pour récupérer les catégories depuis l'API
-export const fetchCategories = async () => {
-  // Effectue une requête pour récupérer les catégories
-  const response = await fetch("http://localhost:5678/api/categories");
-  
-  // Vérifie si la réponse de la requête est OK, sinon lance une erreur avec le statut HTTP
-  if (!response.ok) {
-    throw new Error(`Erreur HTTP! Statut : ${response.status}`);
-  }
-  
-  // Retourne les données des catégories au format JSON
-  return response.json();
-};
+export const fetchCategories = () => fetchData("http://localhost:5678/api/categories");
 
 // Fonction pour envoyer les données d'une nouvelle œuvre
 export const sendWorkData = async (formData) => {
@@ -36,45 +25,33 @@ export const sendWorkData = async (formData) => {
   try {
     const response = await fetch(postWorkUrl, {
       method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`,  // Inclure le token d'authentification dans l'en-tête
-      },
+      headers: { 'Authorization': `Bearer ${token}` },
       body: formData,
     });
 
-    if (!response.ok) {
-      throw new Error('Erreur lors de l\'ajout de la photo');
-    }
+    if (!response.ok) throw new Error('Erreur lors de l\'ajout de la photo');
 
-    const data = await response.json();
-    return data;
+    return await response.json();
   } catch (error) {
     console.error('Error during POST request:', error);
-    if (error instanceof Response) {
-      // Log the details of the error response
-      const responseText = await error.text();
-      console.error('Error response:', responseText);
-    }
-    throw error;
   }
 };
 
+// Fonction pour supprimer une image d'une œuvre
 export const deleteWorkImage = async (workId) => {
   const token = sessionStorage.getItem("token");
+
   try {
     const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`, // Remplacez authToken par votre jeton d'authentification
+        "Authorization": `Bearer ${token}`,
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`La suppression de l'image a échoué. Statut : ${response.status}`);
-    }
+    if (!response.ok) throw new Error(`La suppression de l'image a échoué. Statut : ${response.status}`);
 
-    // Si la réponse n'inclut pas de données JSON valide, ou si le statut est 204, renvoyer null
     return response.status === 204 ? null : await response.json();
   } catch (error) {
     throw new Error(`Erreur lors de la suppression de l'image : ${error.message}`);
--- FrontEnd/js/auth.js
+++ FrontEnd/js/auth.js
@@ -1,42 +1,59 @@
+// Importation des fonctions nécessaires depuis d'autres fichiers
 import { openModal } from "./modal.js";
 import { createFilterButtons } from "./filter.js";
 
-const update = (isLoggedIn) => {
+// Fonction de mise à jour de l'interface utilisateur en fonction de l'état de connexion
+const updateUI = (isLoggedIn) => {
+  // Récupération du bouton de connexion
   const loginButton = document.getElementById("login-button");
+
+  // Mise à jour du texte et du comportement du bouton en fonction de l'état de connexion
   loginButton.textContent = isLoggedIn ? "logout" : "login";
   loginButton.onclick = isLoggedIn ? logout : () => (window.location.href = "login.html");
 
+  // Récupération du conteneur des filtres
   const filterContainer = document.querySelector(".filter");
 
-  if (filterContainer) {
-    if (isLoggedIn) {
-      filterContainer.innerHTML = "";
-    }
+  // Effacement des filtres si l'utilisateur est connecté
+  if (filterContainer && isLoggedIn) {
+    filterContainer.innerHTML = "";
   }
 
+  // Récupération du titre du portfolio
   const portfolioTitle = document.querySelector(".portfolio-title");
 
+  // Ajout d'un bouton de modification si l'utilisateur est connecté
   if (isLoggedIn) {
-    const newButton = document.createElement("button");
-    newButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Modifier';
-    newButton.classList.add("modify");
-    newButton.id = "openModalBtn";
-    newButton.onclick = openModal;
-    portfolioTitle.appendChild(newButton);
+    const modifyButton = document.createElement("button");
+    modifyButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Modifier';
+    modifyButton.classList.add("modify");
+    modifyButton.id = "openModalBtn";
+    modifyButton.onclick = openModal;
+    portfolioTitle.appendChild(modifyButton);
   }
 };
 
+// Fonction de déconnexion
 const logout = () => {
+  // Suppression du jeton de session
   sessionStorage.removeItem("token");
-  update();
-  const addButton = document.getElementById("openModalBtn");
-  if (addButton) {
-    addButton.remove();
+
+  // Mise à jour de l'interface utilisateur
+  updateUI();
+
+  // Suppression du bouton de modification et recréation des boutons de filtre
+  const modifyButton = document.getElementById("openModalBtn");
+  if (modifyButton) {
+    modifyButton.remove();
     createFilterButtons();
   }
 };
 
+// Fonction pour vérifier l'état d'authentification
 export const checkAuthStatus = () => {
+  // Récupération du jeton de session
   const token = sessionStorage.getItem("token");
-  update(token);
+
+  // Mise à jour de l'interface utilisateur en fonction de l'état de connexion
+  updateUI(token);
 };

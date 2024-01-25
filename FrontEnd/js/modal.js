55712cc63dbd88d7a83df0ba08ce17e5dc264655
DOREE VINCENT <vincent.doree01@gmail.com>
2024-01-25 11:56:22 +0100

    feat: add change button color when input, add div for edition mode, add error for image and reinitialise

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -11,6 +11,20 @@ export const openModal = () => {
   // Initialisation du contenu de la modal
   modalContent.innerHTML = "";
 
+  const modalEditionContainer = document.createElement("div");
+  modalEditionContainer.classList.add("modal-edition");
+
+   // Ajout de l'icône et du texte dans la div modal-edition
+   const editModeIcon = document.createElement("i");
+   editModeIcon.classList.add("fa-solid", "fa-pen-to-square");
+   const editModeText = document.createElement("span");
+   editModeText.classList.add("span-text")
+   editModeText.textContent = "Mode édition";
+   modalEditionContainer.appendChild(editModeIcon);
+   modalEditionContainer.appendChild(editModeText);
+
+  modalContent.appendChild(modalEditionContainer);
+
   // Création du titre de la modal
   const modalTitle = document.createElement("h3");
   modalTitle.textContent = "Galerie Photo";
@@ -40,7 +54,7 @@ export const openModal = () => {
 
   // Ajout des éléments à la modal
   modalContent.append(closeButtonContainer, modalTitle, workImagesContainer, addButton);
- 
+
   // Affichage de la modal
   modal.style.display = "flex";
 };
@@ -53,15 +67,29 @@ const openAddPhotoModal = () => {
   // Initialisation du contenu de la modal
   modalContent.innerHTML = "";
 
+  const modalEditionContainer = document.createElement("div");
+  modalEditionContainer.classList.add("modal-edition");
+
+   // Ajout de l'icône et du texte dans la div modal-edition
+   const editModeIcon = document.createElement("i");
+   editModeIcon.classList.add("fa-solid", "fa-pen-to-square");
+   const editModeText = document.createElement("span");
+   editModeText.classList.add("span-text")
+   editModeText.textContent = "Mode édition";
+   modalEditionContainer.appendChild(editModeIcon);
+   modalEditionContainer.appendChild(editModeText);
+
+  modalContent.appendChild(modalEditionContainer);
+
   // Conteneur pour l'icône de suppression
-  const trashIconContainer = document.createElement("div");
-  trashIconContainer.classList.add("trash-icon-container");
+  const pictureIconContainer = document.createElement("div");
+  pictureIconContainer.classList.add("picture-icon-container");
 
   // Icône de suppression
-  const trashIcon = document.createElement("i");
-  trashIcon.classList.add("fa-regular", "fa-image");
-  trashIcon.style.display = "inline";
-  trashIconContainer.appendChild(trashIcon);
+  const pictureIcon = document.createElement("i");
+  pictureIcon.classList.add("fa-regular", "fa-image");
+  pictureIcon.style.display = "inline";
+  pictureIconContainer.appendChild(pictureIcon);
 
   // Conteneur pour les boutons de navigation
   const buttonContainer = document.createElement("div");
@@ -86,7 +114,7 @@ const openAddPhotoModal = () => {
   buttonContainer.appendChild(closeButtonContainer);
 
   // Ajout des éléments à la modal
-  modalContent.append(trashIconContainer, buttonContainer);
+  modalContent.append(pictureIconContainer, buttonContainer);
 
   // Titre de la modal
   const modalTitle = document.createElement("h3");
@@ -99,6 +127,7 @@ const openAddPhotoModal = () => {
 
   // Formulaire d'ajout de photo
   const addPhotoForm = document.createElement("form");
+  addPhotoForm.classList.add("form-container");
   addPhotoForm.addEventListener("submit", handleAddPhoto);
 
   // Label et champ de saisie pour le titre
@@ -112,6 +141,7 @@ const openAddPhotoModal = () => {
   titleInput.name = "title";
   titleInput.placeholder = "";
   titleInput.required = true;
+  titleInput.addEventListener("input", updateSubmitButtonColor);
 
   // Label et liste déroulante pour la catégorie
   const categoryLabel = document.createElement("label");
@@ -122,6 +152,7 @@ const openAddPhotoModal = () => {
   categorySelect.id = "categorySelect";
   categorySelect.name = "category";
   categorySelect.required = true;
+  categorySelect.addEventListener("input", updateSubmitButtonColor);
 
   // Option vide comme première option
   const emptyOption = document.createElement("option");
@@ -195,6 +226,7 @@ const openAddPhotoModal = () => {
         imageInput.style.display = "none";
         addPhotoHelpText.style.display = "none";
         imageInputLabel.style.display = "none";
+        updateSubmitButtonColor(); // Appel pour mettre à jour la couleur du bouton
       };
       reader.readAsDataURL(file);
     }
@@ -204,12 +236,16 @@ const openAddPhotoModal = () => {
   modalContent.append(modalTitle, addPhotoForm);
   formElementsContainer.append(titleLabel, titleInput, categoryLabel, categorySelect);
 
+  // Initialisation de la couleur du bouton
+  submitButton.style.backgroundColor = "#A7A7A7";
+
   // Ajout des éléments pour l'ajout de photo
-  imageInputContainer.append(trashIconContainer, imageInput, imageInputLabel, addPhotoHelpText);
+  imageInputContainer.append(pictureIconContainer, imageInput, imageInputLabel, addPhotoHelpText);
   addPhotoFormContainer.append(imageInputContainer, imagePreviewContainer);
   addPhotoForm.append(addPhotoFormContainer, formElementsContainer, submitButton);
 
   // Affichage de la modal
+  updateSubmitButtonColor();
   modal.style.display = "flex";
 };
 
@@ -221,9 +257,41 @@ const handleAddPhoto = async (event) => {
   const imageInput = document.querySelector('input[name="imageUrl"]');
   const titleInput = document.querySelector('input[name="title"]');
   const categorySelect = document.querySelector('select[name="category"]');
+  const pictureIcon = document.querySelector('.fa-regular.fa-image');
+  const addPhotoHelpText = document.querySelector('.help-text');
+  const imageInputLabel = document.querySelector('.form-label[for="imageInput"]');
+  const imagePreviewContainer = document.getElementById('image-preview');
 
   // Récupération du fichier image
   const imageFile = imageInput.files[0];
+
+   // Vérification du type de fichier
+   if (!imageFile || (imageFile.type !== 'image/jpeg' && imageFile.type !== 'image/png')) {
+    console.error('Veuillez sélectionner un fichier au format JPEG ou PNG.');
+       // Réinitialisation des éléments
+      pictureIcon.style.display = "inline";
+       addPhotoHelpText.style.display = "inline";
+       imageInputLabel.style.display = "initial";
+   
+       // Réinitialisation du champ d'ajout de photo
+       imagePreviewContainer.innerHTML = '';
+   
+       return;
+  }
+
+  // Vérification de la taille du fichier (4 Mo max)
+  if (imageFile.size > 4 * 1024 * 1024) {
+    console.error('La taille du fichier ne doit pas dépasser 4 Mo.');
+       // Réinitialisation des éléments
+       pictureIcon.style.display = "inline";
+       addPhotoHelpText.style.display = "inline";
+       imageInputLabel.style.display = "initial";
+   
+       // Réinitialisation du champ d'ajout de photo
+       imagePreviewContainer.innerHTML = '';
+   
+       return;
+  }
 
   // Création d'un objet FormData avec les données du formulaire
   const formData = new FormData();
@@ -263,3 +331,17 @@ window.addEventListener("click", (event) => {
     closeModal();
   }
 });
+
+// Fonction pour mettre à jour la couleur du bouton en fonction de l'état des champs
+const updateSubmitButtonColor = () => {
+  const titleInput = document.querySelector('input[name="title"]');
+  const categorySelect = document.querySelector('select[name="category"]');
+  const imageInput = document.querySelector('input[name="imageUrl"]');
+  const submitButton = document.querySelector(".submit");
+
+  // Vérifier si tous les champs sont remplis
+  const allFieldsFilled = titleInput.value.trim() !== "" && categorySelect.value.trim() !== "" && imageInput.files.length > 0;
+
+  // Mettre à jour la couleur du bouton en conséquence
+  submitButton.style.backgroundColor = allFieldsFilled ? "#1D6154" : "#A7A7A7";
+};
